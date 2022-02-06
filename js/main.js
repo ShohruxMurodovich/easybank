278ec597cdf87bb4366ce0627bfb47e41e05a881
ShohruxMurodovich <murodovichshohruh@gmail.com>
2022-02-06 15:19:59 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let elHeader = document.querySelector(".header");
+let elHeaderHamburgBtn = document.querySelector(".header-inner__hamburg");
+
+elHeaderHamburgBtn.addEventListener('click', function(){
+  elHeader.classList.toggle("header--active");
+})
