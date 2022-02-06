b23a0d7a1de68e556bbbb12ed4421fc53dac8282
ShohruxMurodovich <murodovichshohruh@gmail.com>
2022-02-06 19:03:54 +0500

    end the project

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ let elHeader = document.querySelector(".header");
 let elHeaderHamburgBtn = document.querySelector(".header-inner__hamburg");
 
 elHeaderHamburgBtn.addEventListener('click', function(){
-  elHeader.classList.toggle("header--active");
+elHeader.classList.toggle("header--active");
 })
